6a6c4460993e8d67ddc139b32f98bdb05a18f775
Tom Vleminckx <tom.vleminckx@clear2pay.com>
2015-11-08 16:16:14 +0100

    code cleanup & shorten the test duration to fit presentation time

--- src/jmh/java/com/devoxx/tia/jmh/ProfilerDemoBench.java
+++ src/jmh/java/com/devoxx/tia/jmh/ProfilerDemoBench.java
@@ -13,27 +13,29 @@ import org.openjdk.jmh.infra.Blackhole;
 public class ProfilerDemoBench {
 
 	private Map<Long, Boolean> map = new HashMap<Long, Boolean>();
-	
-	//private ConcurrentHashMap<Long, Boolean> map = new ConcurrentHashMap<Long, Boolean>();
-	
-	@Param(value={"0","1000"})
+
+	// private ConcurrentHashMap<Long, Boolean> map = new
+	// ConcurrentHashMap<Long, Boolean>();
+
+	@Param(value = { "0", "1000" })
 	private long tokens;
-	
-	
-	public  void doSomething(long id) {
-		
-		Blackhole.consumeCPU(tokens);
+
+	public void doSomething(long id) {
+
 		synchronized (map) {
 			if (!map.containsKey(id))
 				map.put(id, Boolean.TRUE);
-			}
-		//map.putIfAbsent(id, Boolean.TRUE);
+		}
+		// map.putIfAbsent(id, Boolean.TRUE);
 	}
-	
+
 	@Benchmark
 	public void record() {
+
+		Blackhole.consumeCPU(tokens);
+
 		long id = Thread.currentThread().getId();
 		doSomething(id);
 	}
-	
+
 }
